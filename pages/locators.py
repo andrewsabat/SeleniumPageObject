63b8b3c69a4421cbb3fe98f48237275fb27b1117
andrewsabat <austbader@gmail.com>
2021-03-07 16:59:22 +0200

    added login page and locators for login and main pages

---
+++ pages/locators.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    PAGE_ADDRESS = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTER_FORM = (By.ID, "register_form")
